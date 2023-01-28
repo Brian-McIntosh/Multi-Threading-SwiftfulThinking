282e28f08b37b3004143c6cf70655c1fdbd70d80
Brian McIntosh <brian@mixinteractive.com>
2023-01-28 18:35:39 -0500

    Initial Commit

---
+++ Multi-Threading-SwiftfulThinking/Multi_Threading_SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Threading_SwiftfulThinkingApp.swift
+//  Multi-Threading-SwiftfulThinking
+//
+//  Created by Brian McIntosh on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Threading_SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
